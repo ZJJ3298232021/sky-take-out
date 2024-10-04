6d37a9f47e5fd39fbf45e7fa9275913f1f55e210
zank <3298232021@qq.com>
2024-10-05 00:21:54 +0800

    day01完成:     * 将springboot2改为springboot3     * 解决依赖版本问题:         1.knife4j依赖改为适用于JDK17的版本（OpenApi3规范，原先DTO的注解修改为Schema         2.Lombok版本修改为适用于JDK17的版本（Class com.sun.tools.javac.tree.JCTree$JCImport does not have member field 'com.sun.tools.javac.tree.JCTree qualid'）         3.druid版本修改         4.mybatis起步依赖版本修改         5.在yml中配置新版本的Knife4j     * 将WebMvcConfigurationSupport的继承改为WebMvcConfigurer的实现     * 删除静态资源映射和Docket的Bean定义     * 密码不使用MD5加密（不算特别安全）而是采用bcrypt加密     * 修改Jwt登录拦截器使其在未登录时也能有合适的返回结果     * 为PageResult添加泛型限制（单纯为了解决警告）

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
@@ -1,7 +1,11 @@
 package com.sky.interceptor;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.json.JacksonObjectMapper;
 import com.sky.properties.JwtProperties;
+import com.sky.result.Result;
 import com.sky.utils.JwtUtil;
 import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
@@ -9,8 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * jwt令牌校验的拦截器
@@ -28,7 +32,7 @@ public class JwtTokenAdminInterceptor implements HandlerInterceptor {
      * @param request
      * @param response
      * @param handler
-     * @return
+     * @return boolean
      * @throws Exception
      */
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -46,12 +50,15 @@ public class JwtTokenAdminInterceptor implements HandlerInterceptor {
             log.info("jwt校验:{}", token);
             Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
             Long empId = Long.valueOf(claims.get(JwtClaimsConstant.EMP_ID).toString());
-            log.info("当前员工id：", empId);
+            log.info("当前员工id：{}", empId);
             //3、通过，放行
             return true;
         } catch (Exception ex) {
-            //4、不通过，响应401状态码
+            //4、不通过，响应状态码401
             response.setStatus(401);
+            response.setContentType("application/json;charset=utf-8");
+            //todo 利用现有的Json工具替代Gson
+            response.getWriter().write(new Gson().toJson(Result.error("未登录")));
             return false;
         }
     }
